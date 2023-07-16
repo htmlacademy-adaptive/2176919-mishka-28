3f13416cf18928ff52457a5d7f8cf031fbef5a08
Floias <irina.butcher@gmail.com>
2023-07-16 17:06:40 +0500

    адаптив

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import { stacksvg } from 'gulp-stacksvg';
 import autoprefixer from 'autoprefixer';
+import {deleteAsync} from 'del';
 import browser from 'browser-sync';
 
 // Styles
@@ -12,18 +20,98 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe (rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+// HTML
+
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('build'));
+}
+
+// Scripts
+
+const script = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe (rename('script.min.js'))
+    .pipe(gulp.dest('build/js'));
+}
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'))
+}
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(gulp.dest('build/img'))
+}
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh({
+    webp: {}
+  }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// SVG
+
+const svg = () => {
+  return gulp.src(['source/img/**/*.svg', '!source/img/icon/*.svg'])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+}
+
+const makeStack = () => {
+  return gulp.src('source/img/icon/*.svg')
+  .pipe(svgo())
+  .pipe(stacksvg({ separator: `-` }))
+  .pipe(gulp.dest('build/img'));
+}
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+  'source/fonts/*.{woff2,woff}',
+  'source/*.ico',
+  'source/manifest.webmanifest',
+  ], {
+  base: 'source'
+  })
+  .pipe(gulp.dest('build'))
+  done();
+  }
+
+// Clean
+
+const clean = () => {
+  return deleteAsync('build');
+};
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -32,14 +120,49 @@ const server = (done) => {
   done();
 }
 
+// Reload
+
+const reload = (done) => {
+  browser.reload();
+  done();
+  }
+
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
-}
+  gulp.watch('source/js/script.js', gulp.series(script));
+  gulp.watch('source/*.html', gulp.series(html, reload));
+  }
 
+// Build
+
+export const build = gulp.series(
+  clean,
+  gulp.parallel(
+    html,
+    styles,
+    script,
+    svg,
+    makeStack,
+    copy,
+    optimizeImages,
+    createWebp
+  ),
+);
 
 export default gulp.series(
-  styles, server, watcher
+  clean,
+  gulp.parallel(
+    html,
+    styles,
+    script,
+    svg,
+    makeStack,
+    copy,
+    copyImages,
+    createWebp
+  ),
+  server,
+  watcher
 );
---
+++ source/js/script.js
@@ -0,0 +1,44 @@
+const header = document.querySelector('.main-header__container');
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-header__toggle-navigation');
+if (header) {
+  header.classList.remove('main-header__container--nojs');
+}
+
+if (navToggle) {
+  navToggle.addEventListener('click', function () {
+    navMain.classList.toggle('main-navigation--opened');
+  });
+}
+
+const offerButton = document.querySelector('.special-offer__button');
+const modalContainer = document.querySelector('.modal');
+const cardButton = document.querySelector('.product-card__button');
+
+if (offerButton) {
+  offerButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modalContainer.classList.remove('modal--close');
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      modalContainer.classList.add('modal--close');
+    }
+  });
+}
+
+if (cardButton) {
+  cardButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modalContainer.classList.remove('modal--close');
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      modalContainer.classList.add('modal--close');
+    }
+  });
+}
